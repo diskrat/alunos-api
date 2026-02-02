a55d639c7095b066b69ad7bde7d0b403de278bca
Joao Silva <joaodoer3@gmail.com>
2026-02-02 16:01:31 -0300

    feat: add user seeder for initial user data population

---
+++ src/seeders/20260202185027-cria-usuario.js
@@ -0,0 +1,35 @@
+const bcryptjs = require('bcryptjs');
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          nome: 'Luiz',
+          email: 'luiz1@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz 2',
+          email: 'luiz2@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+        {
+          nome: 'Luiz 3',
+          email: 'luiz3@gmail.com',
+          password_hash: await bcryptjs.hash('123456', 8),
+          created_at: new Date(),
+          updated_at: new Date(),
+        },
+      ],
+      {},
+    );
+  },
+
+  async down() {},
+};
