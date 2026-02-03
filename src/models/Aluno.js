114530316e4fdfcb81f4150c4e831c8a920bf87b
Joao Silva <joaodoer3@gmail.com>
2026-02-02 21:39:07 -0300

    refactor: streamline Aluno model validation and remove unnecessary index definition

--- src/models/Aluno.js
+++ src/models/Aluno.js
@@ -4,73 +4,66 @@ export default class Aluno extends Model {
   static init(sequelize) {
     super.init({
       nome: {
-      type: Sequelize.STRING,
-      defaultValue: '',
-      validate: {
-        len: {
-        args: [3, 255],
-        msg: 'Nome precisa ter entre 3 e 255 caracteres.',
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Nome precisa ter entre 3 e 255 caracteres.',
+          },
         },
-      },
       },
       sobrenome: {
-      type: Sequelize.STRING,
-      defaultValue: '',
-      validate: {
-        len: {
-        args: [3, 255],
-        msg: 'Sobrenome precisa ter entre 3 e 255 caracteres.',
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 255],
+            msg: 'Sobren ome precisa ter entre 3 e 255 caracteres.',
+          },
         },
-      },
       },
       email: {
-      type: Sequelize.STRING,
-      defaultValue: '',
-      unique: {
-        name: 'unique_email',
-        msg: 'E-mail já existe',
-      },
-      validate: {
-        isEmail: {
-        msg: 'E-mail inválido',
+        type: Sequelize.STRING,
+        defaultValue: '',
+        unique: {
+          msg: 'E-mail ja existe',
         },
-      },
+        validate: {
+          isEmail: {
+            msg: 'E-mail invalido',
+          },
+        },
       },
       idade: {
-      type: Sequelize.INTEGER,
-      defaultValue: '',
-      validate: {
-        isInt: {
-        msg: 'Idade precisa ser um inteiro.',
+        type: Sequelize.INTEGER,
+        defaultValue: '',
+        validate: {
+          isInt: {
+            msg: 'Idade precisa ser um inteiro.',
+          },
         },
-      },
       },
       peso: {
-      type: Sequelize.FLOAT,
-      defaultValue: '',
-      validate: {
-        isFloat: {
-        msg: 'Peso precisa ser um float',
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Peso precisa ser um float',
+          },
         },
-      },
       },
       altura: {
-      type: Sequelize.FLOAT,
-      defaultValue: '',
-      validate: {
-        isFloat: {
-        msg: 'Altura precisa ser um float',
+        type: Sequelize.FLOAT,
+        defaultValue: '',
+        validate: {
+          isFloat: {
+            msg: 'Altura precisa ser um float',
+          },
         },
-      },
       },
     }, {
       sequelize,
-      indexes: [
-      {
-        unique: true,
-        fields: ['email'],
-      },
-      ],
     });
     return this;
   }
